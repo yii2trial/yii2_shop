1283ea4c0c3a4177daa33304468ad57882a74cf0
Sergei <s@s.ru>
2018-11-10 20:25:43 +0300

    Add install memchache to vagrant
    Added php7.2-memcached php-library installation and memcached server
    in vagrant/provision/once-as-root.sh

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -25,7 +25,7 @@ apt-get update
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.2-curl php7.2-cli php7.2-intl php7.2-mysqlnd php7.2-gd php7.2-fpm php7.2-mbstring php7.2-xml unzip nginx mysql-server-5.7 php.xdebug
+apt-get install -y php7.2-curl php7.2-cli php7.2-intl php7.2-mysqlnd php7.2-gd php7.2-fpm php7.2-mbstring php7.2-xml unzip nginx mysql-server-5.7 php.xdebug php7.2-memcached memcached
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
